6d891a1a8152c2284edc49dbc2538a9f6037e75c
Darwin <darwin.morocho@icloud.com>
2022-12-16 17:00:44 -0500

    init

---
+++ lib/data/repositories_impl/notifications_repository_impl.dart
@@ -0,0 +1,3 @@
+import 'package:streams/domain/repositories/notifications_repository.dart';
+
+class NotificationsRepositoryImpl implements NotificationsRepository {}
---
+++ lib/domain/models/notification.dart
@@ -0,0 +1,11 @@
+class Notification {
+  final String title;
+  final String body;
+  final DateTime createdAt;
+
+  Notification({
+    required this.title,
+    required this.body,
+    required this.createdAt,
+  });
+}
